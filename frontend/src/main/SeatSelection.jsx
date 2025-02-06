1b6485f4b6bc59ebd3bc56eb4a2e74a53aa6f6ff
Anand19987i <anandpandey1765@gmail.com>
2025-02-06 14:51:58 +0530

    commit

--- frontend/src/main/SeatSelection.jsx
+++ frontend/src/main/SeatSelection.jsx
@@ -56,7 +56,7 @@ const SeatSelection = () => {
             });
 
             const options = {
-                key: process.env.RAZORPAY_KEY_ID,
+                key: import.meta.env.VITE_RAZORPAY_KEY_ID,
                 amount: data.order.amount,
                 currency: "INR",
                 name: `${eventDetail?.eventTitle} Seat Booking`,
