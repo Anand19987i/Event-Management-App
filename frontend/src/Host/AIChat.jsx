f9be402c572c2d43795b6c148ce9c4924df491bb
Anand19987i <anandpandey1765@gmail.com>
2025-02-06 12:15:13 +0530

    commit

--- frontend/src/Host/AIChat.jsx
+++ frontend/src/Host/AIChat.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { FaArrowUp, FaInbox, FaCopy } from "react-icons/fa6";
-import { Loader2 } from "lucide-react";
-import { BiDetail } from "react-icons/bi";
-import Navbar from "../components/Navbar";
 import MenuBar from "./MenuBar";
 import { EVENT_API_END_POINT } from "@/utils/constant";
 import ReactMarkdown from "react-markdown";
